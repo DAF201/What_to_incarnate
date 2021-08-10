bb64551ec4aa9208b0959e0904ec34a738b724e9
DAF201 <56328675+DAF201@users.noreply.github.com>
2021-08-10 15:03:35 -0500

    Update multi_processing.py

--- video/multi_processing.py
+++ video/multi_processing.py
@@ -5,7 +5,6 @@ import pyautogui
 import time
 import pathlib
 import scan
-import os
 
 
 PATH = str(pathlib.Path(__file__).parent.resolve())
@@ -40,56 +39,6 @@ def video_cap() -> None:
             switch_counter += 1
 
 
-def covered() -> str:
-    print('今天化身什么呢？')
-    print('1:顶碗人,2:贝极星,3:皇珈骑士, 4:嘉心糖, 5.奶淇淋')
-    user = input()
-
-    if user == '1' or user == '顶碗人':
-        covered = r".\source\bowl.png"
-    elif user == '2' or user == '贝极星':
-        covered = r".\source\star.png"
-    elif user == '3' or user == '皇珈骑士':
-        covered = r".\source\knight.png"
-    elif user == '4' or user == '嘉心糖':
-        covered = r".\source\candy.png"
-    elif user == '5' or user == '奶淇淋':
-        covered = r".\source\icecream.png"
-    elif user == 'asoul' or user == '一个魂':
-        covered = r".\source\A_soul.png"
-    elif user == 'mouse' or user == 'mice' or user == 'rat' or '鼠' in user:
-        covered = r".\source\mouse.png"
-    elif user == '毒唯' or user == 'dw':
-        covered = r".\source\joker.png"
-    elif user == 'carol' or user == 'Carol' or user == '珈乐':
-        covered = r".\source\Carol.png"
-    elif user == 'cr':
-        covered = r".\source\cr.png"
-    elif user == 'alien':
-        covered = r".\source\alien.png"
-    elif user == 'redheel' or user == '高跟鞋':
-        covered = r".\source\high_heel.png"
-    elif user == '流汗黄豆' or user == '😅':
-        covered = r".\source\sweating_soybean.png"
-    elif user == 'hj' or user == '汉奸' or user == 'traitor':
-        covered = r".\source\traitor.png"
-    elif user == 'robot' or user == '机器人':
-        covered = r".\source\robot.png"
-    elif user == 'demon' or user == '恶魔':
-        covered = r".\source\demon.png"
-    elif user == 'microphone' or user == 'jb':
-        covered = r".\source\microphone.png"
-    elif user == 'lama' or user == '羊驼' or user == '阿草':
-        if os.path.isfile(PATH+'\source\lama.png'):
-            covered = r".\source\lama.png"
-        else:
-            print('file missing')
-            covered = r".\source\ybb.png"
-    else:
-        covered = r".\source\ybb.png"
-    return covered
-
-
 def bgm() -> None:
     winsound.PlaySound(PATH+'\source\RHH.wav',
                        winsound.SND_ASYNC | winsound.SND_ALIAS)
@@ -107,7 +56,7 @@ def switch(switch_counter):
 
 
 def main() -> None:
-    result = covered()
+    result = scan.covered()
     p1 = Process(target=video_cap, args=())
     p2 = Process(target=scan.scan, args=(result,))
     p1.start()
